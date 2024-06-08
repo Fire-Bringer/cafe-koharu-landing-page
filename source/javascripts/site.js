3e0cc975aaa442ecb37b18b2024160d0391f1038
Rashad DuPaty <radupaty@gmail.com>
2024-06-08 09:05:33 +0900

    added responsive navbar

---
+++ source/javascripts/site.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+// Responsive Navbar
+const menuIcon = document.querySelector('#menu-icon');
+  const navbar = document.querySelector('.navbar');
+  const navbg = document.querySelector('.nav-bg');
+  menuIcon.addEventListener('click', () => {
+      menuIcon.classList.toggle('bx-x');
+      navbar.classList.toggle('active');
+      navbg.classList.toggle('active');
+  });
+
+});
