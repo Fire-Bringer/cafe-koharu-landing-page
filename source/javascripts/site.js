0ed8e69b392599f9b747789b343138e33df46884
Rashad DuPaty <radupaty@gmail.com>
2024-06-09 10:33:23 +0900

    added best sellers slider

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -10,4 +10,14 @@ const menuIcon = document.querySelector('#menu-icon');
       navbg.classList.toggle('active');
   });
 
+  // Best Sellers Slider
+  document.getElementById('next').onclick = function(){
+    const widthItem = document.querySelector('.item').offsetWidth;
+    document.getElementById('formList').scrollLeft += widthItem;
+  }
+  document.getElementById('prev').onclick = function(){
+    const widthItem = document.querySelector('.item').offsetWidth;
+    document.getElementById('formList').scrollLeft -= widthItem;
+  }
+
 });
