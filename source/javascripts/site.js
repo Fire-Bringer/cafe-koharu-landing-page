c95941629410a8ee053c9dd23c1030e04920dd91
Rashad DuPaty <radupaty@gmail.com>
2024-06-12 21:39:29 +0900

    added splash screen

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,15 +1,49 @@
 document.addEventListener('DOMContentLoaded', function() {
 
+// Splash Screen
+let intro = document.querySelector('.intro');
+let splash = document.querySelector('.splash-header');
+let splashSpan = document.querySelectorAll('.splash');
+
+window.addEventListener('DOMContentLoaded', ()=>{
+
+  setTimeout(()=>{
+
+    splashSpan.forEach((span, idx)=>{
+      setTimeout(()=>{
+        span.classList.add('active');
+      }, (idx + 1) * 800)
+    });
+
+    setTimeout(()=>{
+      splashSpan.forEach((span, idx)=>{
+
+        setTimeout(()=>{
+          span.classList.remove('active');
+          span.classList.add('fade');
+        }, (idx + 1) * 350)
+
+      })
+    },4000);
+
+    setTimeout(()=>{
+      intro.style.top = '-100vh';
+    }, 4300)
+
+  })
+
+})
+
 // Navbar on Scroll
 window.onscroll = function () {
   const docScrollTop = document.documentElement.scrollTop;
 
   if (window.innerWidth > 991) {
       if (docScrollTop > 100) {
-          document.querySelector(".header").classList.add("scrolled")
+          document.querySelector('.header').classList.add('scrolled')
       }
       else {
-          document.querySelector(".header").classList.remove("scrolled")
+          document.querySelector('.header').classList.remove('scrolled')
       }
   }
 }
