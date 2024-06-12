935874d782a3ca3bba70708211e0ea7190010bd2
Rashad DuPaty <radupaty@gmail.com>
2024-06-12 12:33:24 +0900

    finished base design

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,5 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
 
+// Navbar on Scroll
+window.onscroll = function () {
+  const docScrollTop = document.documentElement.scrollTop;
+
+  if (window.innerWidth > 991) {
+      if (docScrollTop > 100) {
+          document.querySelector(".header").classList.add("scrolled")
+      }
+      else {
+          document.querySelector(".header").classList.remove("scrolled")
+      }
+  }
+}
+
 // Responsive Navbar
 const menuIcon = document.querySelector('#menu-icon');
   const navbar = document.querySelector('.navbar');
